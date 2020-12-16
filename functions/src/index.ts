212e5713b1a9a9ea81623296b58eb8f690f53823
Badal Patel <badalpatel6534@gmail.com>
2020-12-16 19:15:02 +0530

    code commenting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,6 +13,18 @@ admin.initializeApp({
 export const updateUserPhoneNumber = functions.https.onCall(
     async (request, context) => {
       try {
+        // step to call callable function from Angular/Ionic
+        // 1) function module import in app.module.ts
+        //    import { AngularFireFunctionsModule } from '@angular/fire/functions';
+        //    imports: [AngularFireFunctionsModule]
+        // 2) Now, import function in class (component/page)
+        //    import { AngularFireFunctions } from '@angular/fire/functions';
+        // 3) inject dependency in constructor
+        //    private functions: AngularFireFunctions
+        // 4) call callable function
+        //    const callable = this.functions.httpsCallable('updateUserPhoneNumber');
+        //    const obs = callable({ uid: this.user.patientId, phoneNumber: '+91xxxxxxxxxx' });
+        //    obs.subscribe(async res => {});
         const userUpdate = await admin.auth().updateUser(request.uid, {
           phoneNumber: request.phoneNumber
         });
